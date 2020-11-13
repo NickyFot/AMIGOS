a830f8b60c1a11c38dc89f5e4bcaf4bc161c5d2d
NickyFot <nfoteinopoulou@gmail.com>
2020-11-13 12:50:29 +0000

    pep8 compliance corrections

--- preprocess/mov_split_frames.py
+++ preprocess/mov_split_frames.py
@@ -14,7 +14,16 @@ def probe_file(filename: str) -> float:
         the length of the video in seconds (float)
 
     """
-    cmnd = ['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', filename]
+    cmnd = [
+        'ffprobe',
+        '-v',
+        'error',
+        '-show_entries',
+        'format=duration',
+        '-of',
+        'default=noprint_wrappers=1:nokey=1',
+        filename
+    ]
     p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print(filename)
     out, err = p.communicate()
@@ -24,9 +33,9 @@ def probe_file(filename: str) -> float:
 def get_segments(duration: float) -> typing.List[tuple]:
     """Get the start and end timestamp in seconds, of the AMIGOS video.
 
-    Each of the videos were split into 20 second clips. For this, the first 20 seconds of each video, including 5 seconds
-    prior to the presentation of the stimuli, were extracted as first clip, then, starting from the 5s of the video
-    (instant in which the stimuli started), n = ⌊(D)/(20s)⌋ non overlapping segments of 20s were extracted,
+    Each of the videos were split into 20 second clips. For this, the first 20 seconds of each video, including 5
+    seconds prior to the presentation of the stimuli, were extracted as first clip, then, starting from the 5s of
+    the video (instant in which the stimuli started), n = ⌊(D)/(20s)⌋ non overlapping segments of 20s were extracted,
     with D being the duration of the stimuli video in seconds.
     Finally, the last 20 seconds of the video were extracted as final clip.
     eg. for an 85s clip
@@ -56,12 +65,12 @@ def seconds_to_strtime(seconds: float) -> str:
     return str(datetime.timedelta(seconds=seconds))
 
 
-def main(dir_path, dst_dir_path, class_name):
-    class_path = os.path.join(dir_path, class_name)
+def main(root_path, dst_dir, class_name):
+    class_path = os.path.join(root_path, class_name)
     if not os.path.isdir(class_path):
         return
 
-    dst_class_path = os.path.join(dst_dir_path, class_name)
+    dst_class_path = os.path.join(dst_dir, class_name)
     if not os.path.exists(dst_class_path):
         os.makedirs(dst_class_path)
 
@@ -83,12 +92,26 @@ def main(dir_path, dst_dir_path, class_name):
                     os.makedirs(dst_directory_path)
                 else:
                     continue
-            except:
-                print(dst_directory_path)
+            except Exception as e:
+                print(dst_directory_path, e)
                 continue
-            segment_strt = segments[idx][0]
-            segment_end = segments[idx][1]
-            cmd = 'ffmpeg -to {2} -i \"{0}\" -vf trim={1}:{2},setpts=PTS-STARTPTS -q:v 5 -f image2 \"{3}/image_%05d.jpg\"'.format(video_file_path, segment_strt, segment_end, dst_directory_path)
+            segment_strt = str(segments[idx][0])
+            segment_end = str(segments[idx][1])
+            cmd = [
+                'ffmpeg',
+                '-to',
+                segment_end,
+                '-i',
+                f'\"{video_file_path}\"',
+                '-vf',
+                f'trim={segment_strt}:{segment_end},setpts=PTS-STARTPTS',
+                '-q:v',
+                '5',
+                '-f',
+                'image2',
+                f'\"{dst_directory_path}/image_%05d.jpg\"'
+            ]
+            cmd = ' '.join(cmd)
             print(cmd)
             subprocess.call(cmd, shell=True)
             print('\n')
@@ -100,5 +123,5 @@ if __name__ == "__main__":
     dst_dir_path = sys.argv[2]
 
     for root, dirs, files in os.walk(dir_path):
-        for class_name in dirs:
-            main(root, dst_dir_path, class_name)
+        for dirname in dirs:
+            main(root, dst_dir_path, dirname)
