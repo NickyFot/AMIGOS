1e05117a4c14579411d0a5179c5a32e83642e91d
NickyFot <nfoteinopoulou@gmail.com>
2020-11-18 12:37:20 +0000

    adding file in path

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
+import sys
+import os
+sys.path.append(os.path.basename(os.getcwd()))
 from .amigos_dataset import AMIGOS
 from . import utils as utils
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.basename(os.getcwd()))
 from .loaders import *
 from .data_helpers import *
 from . import custom_transforms as custom_transforms
